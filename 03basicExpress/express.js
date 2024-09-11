2e2fc8eac1182719e80ab1ebff59340a3988658a
SalmanHossen <hossainsalman873@gmail.com>
2024-09-11 20:01:09 +0600

    finish the most basic concept in express

--- 03basicExpress/express.js
+++ 03basicExpress/express.js
@@ -5,7 +5,8 @@ const app = express();
 app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-   res.render('index')
+   // res.render('index')
+   throw Error('i do not know')
 })
 app.get('/contact',function(req,res){
    res.render('contact',{name:'salman'})
@@ -13,8 +14,19 @@ app.get('/contact',function(req,res){
 app.get('/about',function(req,res){
    res.render('about')
 })
+app.get('/error',function(req,res,next){
+   throw Error('something went wrong')
+})
 
 app.set('view engine','ejs');
 app.use(express.static('./public'));
+
+app.use(function errorHandler (err, req, res, next) {
+   if (res.headersSent) {
+     return next(err)
+   }
+   res.status(500)
+   res.render('error', { error: err })
+ })
 
 app.listen(3000)
